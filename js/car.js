e2f0ae94a47d1f3b328a60f15f59f17e45e55fc2
Goncalao <goncalo.cool@gmail.com>
2017-09-28 11:32:30 +0100

    Acceleration complete, started turning

--- js/car.js
+++ js/car.js
@@ -9,18 +9,32 @@ class Carro
 		this.material = new THREE.MeshBasicMaterial( {color: 0x00FF00, wireframe: true} );
 
 		this.velocity = new THREE.Vector3(0,0,0);
-		this.maxSpeed = 0.02;
-		this.acceleration = 100;
-		//Translates player's input
-		this.thrust = 0;
-		//Makes the car slow to a halt
-		this.drag = 0.6;
 
-		//Texto de debug
+		this.maxSpeed = 0.1;
+		this.speed = 100;
+		this.acceleration = 1.6;
+		//Translates player's throttle input (1 = accelerate, -1 = brake)
+		this.throttle = 0;
+		//Makes the car slow to a halt
+		this.speedDrag = 0.6;
+
+		this.steeringSensitivity = 20;
+		//Translates player's turn input (1 = right, -1 = left)
+		this.turn = 0;
+		//Makes the car turn to the center
+		this.turnDrag = 0.8;
+
+		//Tracks the key's pressed state
+		this.throttlePressed = false;
+		this.brakePressed = false;
+		this.leftPressed = false;
+		this.rightPressed = false;
+
+		//Debug Text
 		this.VelXText;
-		this.ThrustText;
-		this.VelSign;
-		this.ThrSign;
+		this.VelZText;
+		this.throttleText;
+		this.turnText;
 	}
 
 	Start() {
@@ -42,36 +56,52 @@ class Carro
 
 	Update(delta) {
 
-		var throttle = (this.thrust * this.acceleration) * delta;
-		var velocitySign = Math.sign(this.velocity.x);
-		var throttleSign = Math.sign(this.thrust);
+		//Acceleration
+			var thrust = (this.throttle * this.speed) * delta;
+			var velocitySign = Math.sign(this.velocity.x);
+			var throttleSign = Math.sign(this.throttle);
 
-		//Check if car hasn't hit full speed, if it did, don't update velocity
-		if (Math.abs(this.velocity.x) < this.maxSpeed || throttleSign != velocitySign) {
-			this.velocity.x += throttle;
-		}
+			//Check if car hasn't hit full speed, if it did, don't update velocity
+			if (Math.abs(this.velocity.x) < this.maxSpeed || (throttleSign != velocitySign)) {
+				if (Math.abs(this.throttle) == 1) {
+					this.velocity.x += thrust*this.acceleration;
+				}
+				else {
+					this.velocity.x += thrust;
+				}
+			}
 
-		//When velocity is nearly 0, halt the car
-		if (Math.abs(this.throttle) != 1 && Math.abs(this.velocity.x) < 0.0005) {
-			this.velocity = new THREE.Vector3(0,0,0);
-			this.thrust = 0;
-		}
+			//When velocity is nearly 0, and the car isn't at full throttle, halt the car
+			if (this.velocity.x != 0 && Math.abs(this.throttle) != 1 && Math.abs(this.velocity.x) < 0.0005) {
+				this.velocity.x = 0;
+				this.throttle = 0;
+			}
+
+		//Turning
+			var turning = (this.turn * this.steeringSensitivity) * delta;
+			this.velocity.z += turning;
+
+			//When turning is nearly 0, and the player isn't turning, center the steering of the car
+			if (this.velocity.z != 0 && Math.abs(this.turn) != 1 && Math.abs(this.velocity.z) < 0.0005) {
+				this.velocity.z = 0;
+				this.turn = 0;
+			}
 
 		this.ApplyVelocity();
 
 		//Update text
 		this.VelXText.innerHTML = this.velocity.x;
-		this.ThrustText.innerHTML = this.thrust;
-		this.VelSign.innerHTML = velocitySign;
-		this.ThrSign.innerHTML = throttleSign;
+		this.VelZText.innerHTML = this.velocity.z;
+		this.throttleText.innerHTML = this.throttle;
+		this.turnText.innerHTML = this.turn;
 
-		this.car.rotation.y += 0.005;
-		this.car.rotation.x += 0.005;
-		this.car.rotation.z += 0.005;
+		//this.car.rotation.y += 0.005;
+		//this.car.rotation.x += 0.005;
+		//this.car.rotation.z += 0.005;
 	}
 
 	ApplyVelocity() {
-		//car.position += this.velocity;
+		//this.car.position += this.velocity;
 	}
 
 	CreateScreenText() {
@@ -84,73 +114,151 @@ class Carro
 		this.VelXText.style.top = 50 + 'px';
 		this.VelXText.style.left = 100 + 'px';
 		document.body.appendChild(this.VelXText);
-		this.ThrustText = document.createElement('div');
-		this.ThrustText.style.position = 'absolute';
-		this.ThrustText.style.width = 100;
-		this.ThrustText.style.height = 100;
-		this.ThrustText.style.backgroundColor = "white";
-		this.ThrustText.innerHTML = "hi there!";
-		this.ThrustText.style.top = 70 + 'px';
-		this.ThrustText.style.left = 100 + 'px';
-		document.body.appendChild(this.ThrustText);
-		this.VelSign = document.createElement('div');
-		this.VelSign.style.position = 'absolute';
-		this.VelSign.style.width = 100;
-		this.VelSign.style.height = 100;
-		this.VelSign.style.backgroundColor = "white";
-		this.VelSign.innerHTML = "hi there!";
-		this.VelSign.style.top = 90 + 'px';
-		this.VelSign.style.left = 100 + 'px';
-		document.body.appendChild(this.VelSign);
-		this.ThrSign = document.createElement('div');
-		this.ThrSign.style.position = 'absolute';
-		this.ThrSign.style.width = 100;
-		this.ThrSign.style.height = 100;
-		this.ThrSign.style.backgroundColor = "white";
-		this.ThrSign.innerHTML = "hi there!";
-		this.ThrSign.style.top = 110 + 'px';
-		this.ThrSign.style.left = 100 + 'px';
-		document.body.appendChild(this.ThrSign);
+		this.VelZText = document.createElement('div');
+		this.VelZText.style.position = 'absolute';
+		this.VelZText.style.width = 100;
+		this.VelZText.style.height = 100;
+		this.VelZText.style.backgroundColor = "white";
+		this.VelZText.innerHTML = "hi there!";
+		this.VelZText.style.top = 70 + 'px';
+		this.VelZText.style.left = 100 + 'px';
+		document.body.appendChild(this.VelZText);
+		this.throttleText = document.createElement('div');
+		this.throttleText.style.position = 'absolute';
+		this.throttleText.style.width = 100;
+		this.throttleText.style.height = 100;
+		this.throttleText.style.backgroundColor = "white";
+		this.throttleText.innerHTML = "hi there!";
+		this.throttleText.style.top = 90 + 'px';
+		this.throttleText.style.left = 100 + 'px';
+		document.body.appendChild(this.throttleText);
+		this.turnText = document.createElement('div');
+		this.turnText.style.position = 'absolute';
+		this.turnText.style.width = 100;
+		this.turnText.style.height = 100;
+		this.turnText.style.backgroundColor = "white";
+		this.turnText.innerHTML = "hi there!";
+		this.turnText.style.top = 110 + 'px';
+		this.turnText.style.left = 100 + 'px';
+		document.body.appendChild(this.turnText);
+
 	}
 
-	OnThrust() {
-		if (this.thrust < 1) {
-			if (Math.abs(this.thrust) != this.drag) {
-				this.thrust += 1;
-			}
-			else {
-				this.thrust = 1;
+	OnAccelerate() {
+		if (!this.throttlePressed) {
+			this.throttlePressed = true;
+
+			this.throttle = Math.abs(this.throttle) != this.speedDrag ? this.throttle + 1 : 1;
+
+			//If the car's x speed is not 0 and the player is applying no thrust, apply drag
+			if (this.velocity.length != 0 && this.throttle == 0) {
+				this.throttle += (this.speedDrag) * Math.sign(-this.velocity.x);
 			}
 		}
 	}
 
-	OnUnthrust() {
-		this.thrust -= 1;
+	OnUnaccelerate() {
+		this.throttlePressed = false;
+		this.throttle -= 1;
 
 		//If the car's speed is not 0 and the player is applying no thrust, apply drag
-		if (this.velocity.length != 0 && this.thrust == 0) {
-			this.thrust -= (this.drag) * Math.sign(this.velocity.x);
+		if (this.velocity.length != 0 && this.throttle == 0) {
+			this.throttle -= (this.speedDrag) * Math.sign(this.velocity.x);
 		}
+
+		//Make sure thrust doesn't have and absolute value greater than 1
+		this.throttle = this.throttle < -1 ? -1 : this.throttle;
+
+		//Make sure the player uses full throttle if still braking
+		this.throttle = this.brakePressed && this.throttle != -1 ? -1 : this.throttle;
 	}
 
 	OnBrake() {
-		if (this.thrust > -1) {
-			if (Math.abs(this.thrust) != this.drag) {
-				this.thrust -= 1;
-			}
-			else {
-				this.thrust = -1;
+		if (!this.brakePressed) {
+			this.brakePressed = true;
+
+			this.throttle = Math.abs(this.throttle) != this.speedDrag ? this.throttle - 1 : -1;
+
+			//If the car's x speed is not 0 and the player is applying no thrust, apply drag
+			if (this.velocity.length != 0 && this.throttle == 0) {
+				this.throttle += (this.speedDrag) * Math.sign(-this.velocity.x);
 			}
 		}
 	}
 
 	OnUnbrake() {
-		this.thrust += 1;
+		this.brakePressed = false;
+		this.throttle += 1;
 		//If the car's x speed is not 0 and the player is applying no thrust, apply drag
-		if (this.velocity.length != 0 && this.thrust == 0) {
-
-			this.thrust += (this.drag) * Math.sign(-this.velocity.x);
+		if (this.velocity.length != 0 && this.throttle == 0) {
+			this.throttle += (this.speedDrag) * Math.sign(-this.velocity.x);
 		}
+
+		//Make sure thrust doesn't have and absolute value greater than 1
+		this.throttle = this.throttle > 1 ? 1 : this.throttle;
+
+		//Make sure the player uses full throttle if still accelerating
+		this.throttle = this.brakePressed && this.throttle != 1 ? 1 : this.throttle;
+	}
+
+	OnLeft() {
+		if (!this.leftPressed) {
+			this.leftPressed = true;
+
+			this.turn = Math.abs(this.turn) != this.turnDrag ? this.turn - 1 : -1;
+
+			//If the car's x speed is not 0 and the player is applying no thrust, apply drag
+			if (this.velocity.length != 0 && this.turn == 0) {
+				this.turn += (this.turnDrag) * Math.sign(-this.velocity.z);
+			}
+		}
+	}
+
+	OnUnleft() {
+		this.leftPressed = false;
+
+		this.turn -= 1;
+
+		//If the car's speed is not 0 and the player is applying no thrust, apply drag
+		if (this.velocity.length != 0 && this.turn == 0) {
+			this.turn -= (this.turnDrag) * Math.sign(this.velocity.z);
+		}
+
+		//Make sure thrust doesn't have and absolute value greater than 1
+		this.turn = this.turn > 1 ? 1 : this.turn;
+
+		//Make sure the player uses turns right fully
+		this.turn = this.brakePressed && this.turn != 1 ? 1 : this.turn;
+	}
+
+	OnRight() {
+		if (!this.rightPressed) {
+			this.rightPressed = true;
+
+			this.turn = Math.abs(this.turn) != this.turnDrag ? this.turn + 1 : 1;
+
+			//If the car's x speed is not 0 and the player is applying no thrust, apply drag
+			if (this.velocity.length != 0 && this.turn == 0) {
+				this.turn += (this.turnDrag) * Math.sign(-this.velocity.z);
+			}
+		}
+	}
+
+	OnUnright() {
+		this.leftPressed = false;
+
+		this.turn += 1;
+
+		//If the car's speed is not 0 and the player is applying no thrust, apply drag
+		if (this.velocity.length != 0 && this.turn == 0) {
+			this.turn -= (this.turnDrag) * Math.sign(this.velocity.z);
+		}
+
+		//Make sure thrust doesn't have and absolute value greater than 1
+		this.turn = this.turn < -1 ? -1 : this.turn;
+
+		//Make sure the player uses turns left fully
+		this.turn = this.brakePressed && this.turn != -1 ? -1 : this.turn;
 	}
 
 	CreateMiddlePart(x,y,z){
